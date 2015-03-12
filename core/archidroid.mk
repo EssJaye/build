4ac37c6ff7f5def9e7065872232f653a24aeb31c
Łukasz Domeradzki <JustArchi@JustArchi.net>
2015-03-12 16:10:51 +0100

    x

--- core/archidroid.mk
+++ core/archidroid.mk
@@ -30,6 +30,10 @@ ARCHIDROID_GCC_CFLAGS_THUMB := -O3
 # Additional flags passed to all C targets compiled with GCC
 ARCHIDROID_GCC_CFLAGS := -Wno-error=array-bounds -Wno-error=clobbered -Wno-error=maybe-uninitialized -Wno-error=strict-overflow
 
+# If your arm-linux-androideabi includes support for graphite optimization flags (CLooG), enable additional flags
+# You should set -ftree-parallelize-loops=N flag to number of the number of your threads (cores) of your CPU
+ARCHIDROID_GCC_CFLAGS += -fgraphite-identity -floop-block -floop-interchange -floop-nest-optimize -floop-strip-mine -floop-parallelize-all -ftree-parallelize-loops=4
+
 # Flags passed to all C++ targets compiled with GCC
 ARCHIDROID_GCC_CPPFLAGS := -O3 $(ARCHIDROID_CFLAGS)
 
